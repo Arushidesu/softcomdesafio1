c8d10673c5ff7d9b743b142e62df4929537d3124
Alcides Dias <cidesjour13@gmail.com>
2019-05-30 21:46:59 -0300

    Mensagens de erro

--- JsonParse/Models/Usuario.cs
+++ JsonParse/Models/Usuario.cs
@@ -11,9 +11,9 @@ namespace JsonParse.Models
         [Key]
         public int UsuarioId { get; set; }
         public string Nome { get; set; }
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "Esse não é um e-mail válido!")]
         public string Email { get; set; }
-        [Phone]
+        [Phone(ErrorMessage = "Esse não é um telefone válido!")]
         public string Fone { get; set; }
         public List<Conta> Contas { get; set; }
     }
