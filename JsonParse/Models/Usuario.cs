a15bd38a940729c43bf72777fc67f01fe9a5c296
Alcides Dias <cidesjour13@gmail.com>
2019-05-30 21:39:51 -0300

    Email and phone validations

--- JsonParse/Models/Usuario.cs
+++ JsonParse/Models/Usuario.cs
@@ -11,7 +11,9 @@ namespace JsonParse.Models
         [Key]
         public int UsuarioId { get; set; }
         public string Nome { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Phone]
         public string Fone { get; set; }
         public List<Conta> Contas { get; set; }
     }
